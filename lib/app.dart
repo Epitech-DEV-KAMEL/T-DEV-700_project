98ba72196680dcc874e0af29d5e8aac0273dcfa0
Alphamplyer <even@pouyadou.fr>
2021-11-22 12:07:48 +0100

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class TerminalApp extends StatefulWidget {
+  const TerminalApp({ Key? key }) : super(key: key);
+
+  @override
+  _TerminalAppState createState() => _TerminalAppState();
+}
+
+class _TerminalAppState extends State<TerminalApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:terminal_app/app.dart';
+
+void main() {
+  runApp(const TerminalApp());
+}
