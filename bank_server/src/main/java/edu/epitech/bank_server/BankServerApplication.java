e66b2047e8f1ae6f7020cd629647d32e74d9bcb8
Alphamplyer <epouyadou@kaliop.com>
2021-11-26 12:21:13 +0100

    package: add springdoc-openapi-ui mapstruct + rename org.epitech to edu.epitech + change maven build settings

--- bank_server/src/main/java/edu/epitech/bank_server/BankServerApplication.java
+++ bank_server/src/main/java/edu/epitech/bank_server/BankServerApplication.java
@@ -1,4 +1,4 @@
-package org.epitech.bank_server;
+package edu.epitech.bank_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- bank_server/src/test/java/edu/epitech/bank_server/BankServerApplicationTests.java
+++ bank_server/src/test/java/edu/epitech/bank_server/BankServerApplicationTests.java
@@ -1,4 +1,4 @@
-package org.epitech.bank_server;
+package edu.epitech.bank_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
