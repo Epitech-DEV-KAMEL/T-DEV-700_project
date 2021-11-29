6c4369347c58c4bacfb1dedacb3739d65ec08b6b
Alphamplyer <epouyadou@kaliop.com>
2021-11-29 14:59:29 +0100

    refractor: remove unnecessary field

--- terminal_app/lib/core/entities/article.dart
+++ terminal_app/lib/core/entities/article.dart
@@ -6,14 +6,12 @@ class Article extends Equatable {
   final String name;
   final String description;
   final double price;
-  final bool isAvailable;
 
   const Article({
     required this.id,
     required this.name,
     required this.description,
     required this.price,
-    required this.isAvailable
   });
 
   @override
