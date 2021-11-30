1669d74615ee0d3817ed68b71c5192d3c06f60eb
Alphamplyer <epouyadou@kaliop.com>
2021-11-30 11:32:31 +0100

    feat: modify cartArticle and add cart

---
+++ terminal_app/lib/features/domain/entities/cart.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:terminal_app/core/entities/article.dart';
+import 'package:terminal_app/features/domain/entities/cart_article.dart';
+
+class Cart extends ChangeNotifier {
+  final List<CartArticle> _articles = [];
+
+  double get totalPrice => _articles.fold(0, (total, current) => total + (current.price * current.amount));
+
+  void add(Article article, int amount) {
+    assert(amount > 0);
+
+    int articleIndex = _articles.indexWhere((element) => element.id == article.id);
+    bool alreadyInCart = articleIndex != -1;
+
+    if (alreadyInCart) {
+      CartArticle cartArticle = _articles[articleIndex];
+      _articles[articleIndex] = cartArticle.add(amount);
+    } else {
+      _articles.add(CartArticle.fromEntity(article, amount));
+    }
+
+    notifyListeners();
+  }
+
+  void updateCartArticleAmount(CartArticle article, int amount) {
+    assert(amount >= 0);
+
+    if (amount == 0) {
+      removeCartArticle(article);
+    } else {
+      int articleIndex = _articles.indexOf(article);
+      CartArticle cartArticle = _articles[articleIndex];
+      _articles[articleIndex] = cartArticle.copyWith(amount: amount);
+    }
+
+    notifyListeners();
+  }
+
+  void removeCartArticle(CartArticle cartArticle) {
+    _articles.remove(cartArticle);
+    notifyListeners();
+  }
+
+  void clear() {
+    _articles.clear();
+    notifyListeners();
+  }
+}
--- terminal_app/lib/features/domain/entities/cart_article.dart
+++ terminal_app/lib/features/domain/entities/cart_article.dart
@@ -1,5 +1,6 @@
 
 import 'package:equatable/equatable.dart';
+import 'package:terminal_app/core/entities/article.dart';
 
 class CartArticle extends Equatable {
   final int id;
@@ -15,6 +16,36 @@ class CartArticle extends Equatable {
     required this.price, 
     required this.amount
   });
+
+  static CartArticle fromEntity(Article article, int amount) {
+    return CartArticle(
+      id: article.id,
+      name: article.name,
+      description: article.description,
+      price: article.price,
+      amount: amount
+    );
+  }
+
+  CartArticle copyWith({int? amount}) {
+    return CartArticle(
+      id: id,
+      name: name,
+      description: description,
+      price: price,
+      amount: amount ?? this.amount
+    );
+  }
+
+  CartArticle add(int amount) {
+    return CartArticle(
+      id: id,
+      name: name,
+      description: description,
+      price: price,
+      amount: this.amount + amount
+    );
+  }
 
   @override
   List<Object?> get props => [id, amount];  
