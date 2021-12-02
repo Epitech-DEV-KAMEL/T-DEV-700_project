d1debd6ffa3bda3862411aca39a82d988e38b2fd
datVaulting <meskine.momo@gmail.com>
2021-12-02 04:02:38 +0100

    add dockerfile back & new docker compose + conf

--- bank_server/src/main/java/edu/epitech/bank_server/BankServerApplication.java
+++ bank_server/src/main/java/edu/epitech/bank_server/BankServerApplication.java
@@ -2,12 +2,13 @@ package edu.epitech.bank_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+
 public class BankServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BankServerApplication.class, args);
     }
-
 }
